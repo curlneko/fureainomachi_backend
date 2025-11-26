3881e5911f7f439b27d9f5c0ca8f6397749ecbbc
yufangling <helen831128@gmail.com>
2025-11-26 18:45:23 +0900

    init API UI

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="Fureainomachi API", version="0.1.0")
 
 
-@app.get("/")
+@app.get("/home")
 def read_root():
     return {"message": "Hello World"}
