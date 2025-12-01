b8833edfadee0c383b0df5492f8d63487535532d
yufangling <helen831128@gmail.com>
2025-12-01 15:34:14 +0900

    init seed

--- app/main.py
+++ app/main.py
@@ -1,10 +1,34 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
 from app.db.database import Base, engine
+from app.db.seed import init_dummy_data
 from app.routers.posts import router as posts_router
 
-# modelsに定義されたテーブルを作成
-Base.metadata.create_all(bind=engine)
+# .env ファイルの内容を読み込む
+load_dotenv()
 
-app = FastAPI(title="Fureainomachi API", version="0.1.0")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # modelsに定義されたテーブルを作成
+    Base.metadata.create_all(bind=engine)
+
+    # Dummy データ投入
+    if os.getenv("ENV") == "DEV":
+        init_dummy_data()
+
+    # アプリ起動時の処理
+    yield
+
+    # アプリ終了時の処理
+    print("アプリ終了: DB コネクションやバックグラウンドタスクを閉じます")
+    engine.dispose()
+
+
+app = FastAPI(title="Fureainomachi API", version="0.1.0", lifespan=lifespan)
+
 
 app.include_router(posts_router, prefix="/posts", tags=["posts"])
