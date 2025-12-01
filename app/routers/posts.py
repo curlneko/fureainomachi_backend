56cd376933953187abbc0a8c30ab94f5f2bec156
yufangling <helen831128@gmail.com>
2025-12-01 16:22:26 +0900

    feat: create post

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -3,15 +3,21 @@ from sqlalchemy.orm import Session
 from app.db.database import get_db
 
 
-from app.schemas.posts import Post
-from app.services.posts import get_posts
+from app.schemas.posts import PostCreate, PostGet
+from app.services.posts import get_posts_service, create_post_service
 
 router = APIRouter()
 
 
-@router.get("/posts", response_model=list[Post])
+@router.get("", response_model=list[PostGet])
 def get_posts(db: Session = Depends(get_db)):
-    posts = get_posts(db)
+    posts = get_posts_service(db)
     if not posts:
         raise HTTPException(status_code=404, detail="Posts not found")
     return posts
+
+
+@router.post("", response_model=PostGet)
+def create_post(post: PostCreate, db: Session = Depends(get_db)):
+    new_post = create_post_service(db, post)
+    return new_post
